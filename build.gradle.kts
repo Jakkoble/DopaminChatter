94089f3271f5acf43096685b18068a646cfd8f89
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-10-18 15:58:08 +0200

    Added Serialization Dependency ⚙️

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.20"
+    kotlin("plugin.serialization") version "1.7.20"
 }
 
 group = "de.jakkoble"
@@ -13,6 +14,7 @@ repositories {
 
 dependencies {
     implementation("com.github.twitch4j:twitch4j:1.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.2.10")
 }
 
