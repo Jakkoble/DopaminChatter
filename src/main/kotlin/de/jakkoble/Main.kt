159cb4cec92973bd294d5ac4861d7fe335a4546f
Jakkoble <business@jakkoble.de>
2022-10-18 19:08:12 +0200

    Added Feedback Message 📰

--- src/main/kotlin/de/jakkoble/Main.kt
+++ src/main/kotlin/de/jakkoble/Main.kt
@@ -1,9 +1,11 @@
 package de.jakkoble
 
 import de.jakkoble.modules.core.TwitchBot
+import de.jakkoble.utils.ConsoleLogger
 
 fun main() {
    val twitchBot = TwitchBot
    twitchBot.registerEvents()
    twitchBot.start()
+   ConsoleLogger.logInfo("Bot is now Ready an is listening for ChannelEvents...")
 }
