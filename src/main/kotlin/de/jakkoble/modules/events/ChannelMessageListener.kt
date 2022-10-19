10b520a56165097e37f9ca94bec0f189cb57c110
Jakkoble <business@jakkoble.de>
2022-10-19 20:54:26 +0200

    Added Emote Functionality 😎

--- src/main/kotlin/de/jakkoble/modules/events/ChannelMessageListener.kt
+++ src/main/kotlin/de/jakkoble/modules/events/ChannelMessageListener.kt
@@ -3,7 +3,10 @@ package de.jakkoble.modules.events
 import com.github.philippheuer.events4j.simple.SimpleEventHandler
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent
 import de.jakkoble.modules.commands.CommandManager
+import de.jakkoble.modules.core.TwitchBot
+import de.jakkoble.modules.data.ChannelData
 import de.jakkoble.modules.data.UserData
+import de.jakkoble.modules.data.getChannelDataByID
 import de.jakkoble.utils.ConsoleLogger
 
 class ChannelMessageListener(eventHandler: SimpleEventHandler) {
@@ -14,11 +17,25 @@ class ChannelMessageListener(eventHandler: SimpleEventHandler) {
    private fun onChannelMessage(event: ChannelMessageEvent) {
       val message = event.message.split(" ")
       if (message.isEmpty()) return
+      if (message.first() != "#") {
+         handleEmote(getChannelDataByID(event.channel.id) ?: return, message)
+         return
+      }
       if (event.channel.id != event.user.id || event.user.id != "205919808") return
       CommandManager().getCommand(message[0])?.executeCommand(
          channel = UserData(event.channel.name, event.channel.id),
          sender = UserData(event.user.name, event.user.id),
          args = message.subList(1, message.size)
       )
+   }
+   private fun handleEmote(channelData: ChannelData, message: List<String>) {
+      val emoteList = channelData.customEmotes
+      message.forEach {
+         if (emoteList.contains(it)) {
+            if ((1..100).random() <= channelData.writingChance)
+               TwitchBot.twitchClient.chat.sendMessage(channelData.userData.name, it)
+            return
+         }
+      }
    }
 }
