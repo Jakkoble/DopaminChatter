8e723f7e753f10b6bb8cf4bb7c5dcc0a7ad06ea9
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-10-19 10:56:26 +0200

    Added DataManager Functionality 🧾

--- src/main/kotlin/de/jakkoble/modules/data/DataManager.kt
+++ src/main/kotlin/de/jakkoble/modules/data/DataManager.kt
@@ -1,24 +1,31 @@
 package de.jakkoble.modules.data
 
+import de.jakkoble.utils.ConsoleLogger
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
 
 object DataManager {
-   private const val filePath = "channelData.json"
+   const val filePath = "channelData.json"
    init {
       val file = File(filePath)
       if (!file.exists()) {
+         ConsoleLogger.logWarning("channelData.json does not exist!")
          file.createNewFile()
+         channels.add(ChannelData(UserData("Jakkoble", "205919808")))
          file.writeText(Json.encodeToString(channels))
+         ConsoleLogger.logInfo("Created channelData.json file.")
       }
    }
    fun load() {
+      ConsoleLogger.logInfo("Loading data from channelData.json...")
       val data = Json.decodeFromString<MutableList<ChannelData>?>(File(filePath).readText())
       channels.addAll(data ?: return)
+      ConsoleLogger.logInfo("Successfully loaded all data from channelData.json.")
    }
    fun updateChannelData() {
-
+      File(filePath).writeText(Json.encodeToString(channels))
+      ConsoleLogger.logInfo("Updated data in channelData.json.")
    }
 }
