2368b37c9df9a1018911db9412b16253f3b5821c
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-10-19 10:56:06 +0200

    Registered RemoveChannelCommand ✅

--- src/main/kotlin/de/jakkoble/modules/commands/CommandManager.kt
+++ src/main/kotlin/de/jakkoble/modules/commands/CommandManager.kt
@@ -1,11 +1,13 @@
 package de.jakkoble.modules.commands
 
 import de.jakkoble.modules.commands.cmd.AddChannelCommand
+import de.jakkoble.modules.commands.cmd.RemoveChannelCommand
 
 private val commands: MutableList<TwitchCommand> = mutableListOf()
 class CommandManager {
    fun registerCommands() {
       commands.add(AddChannelCommand())
+      commands.add(RemoveChannelCommand())
    }
-   fun getCommand(command: String): TwitchCommand? = commands.firstOrNull { it.command == command }
+   fun getCommand(command: String): TwitchCommand? = commands.firstOrNull { it.command.equals(command, true) }
 }
