8bd827fddcc01fd8e7a792ad34134e14b7bbbaf9
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-10-19 08:46:14 +0200

    Added new Command ✅

---
+++ src/main/kotlin/de/jakkoble/modules/commands/cmd/AddChannelCommand.kt
@@ -0,0 +1,10 @@
+package de.jakkoble.modules.commands.cmd
+
+import de.jakkoble.modules.commands.TwitchCommand
+import de.jakkoble.modules.data.UserData
+
+class AddChannelCommand : TwitchCommand("addchannel") {
+   override fun onCommand(channel: UserData, userData: UserData, args: List<String>) {
+      TODO("Not yet implemented")
+   }
+}
