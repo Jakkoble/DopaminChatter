f2f767964c64c308bd45f0da8670d9497b74e130
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-10-19 09:31:22 +0200

    Added new Command ⚙️

---
+++ src/main/kotlin/de/jakkoble/modules/commands/cmd/RemoveChannelCommand.kt
@@ -0,0 +1,21 @@
+package de.jakkoble.modules.commands.cmd
+
+import de.jakkoble.modules.commands.TwitchCommand
+import de.jakkoble.modules.core.TwitchBot
+import de.jakkoble.modules.data.DataManager
+import de.jakkoble.modules.data.UserData
+import de.jakkoble.modules.data.channels
+import de.jakkoble.utils.ConsoleLogger
+
+class RemoveChannelCommand: TwitchCommand("removeChannel", true) {
+   override fun onCommand(channel: UserData, sender: UserData, args: List<String>) {
+      if (args.isEmpty()) return
+      val channelID = TwitchBot.getChannelID(args[0])
+      if (channelID == null || channels.none { it.userData.id == channelID }) {
+         ConsoleLogger.logWarning("No User Found with the Name ${args[0]}!")
+         return
+      }
+      channels.removeIf { it.userData.id == channelID }
+      DataManager.updateChannelData()
+   }
+}
