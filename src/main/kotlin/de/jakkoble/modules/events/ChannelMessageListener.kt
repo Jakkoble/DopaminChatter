9af032720c9845462037f73501f7828e318c3260
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-10-19 10:55:04 +0200

    Added modular Command Functionality 📰

--- src/main/kotlin/de/jakkoble/modules/events/ChannelMessageListener.kt
+++ src/main/kotlin/de/jakkoble/modules/events/ChannelMessageListener.kt
@@ -2,12 +2,23 @@ package de.jakkoble.modules.events
 
 import com.github.philippheuer.events4j.simple.SimpleEventHandler
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent
+import de.jakkoble.modules.commands.CommandManager
+import de.jakkoble.modules.data.UserData
+import de.jakkoble.utils.ConsoleLogger
 
 class ChannelMessageListener(eventHandler: SimpleEventHandler) {
    init {
       eventHandler.onEvent(ChannelMessageEvent::class.java, this::onChannelMessage)
+      ConsoleLogger.logInfo("Successfully registered ChannelMessageEvent.")
    }
    private fun onChannelMessage(event: ChannelMessageEvent) {
-      event.twitchChat.sendMessage(event.channel.name, event.message)
+      val message = event.message.split(" ")
+      if (message.isEmpty()) return
+      if (event.channel.id != event.user.id || event.user.id != "205919808") return
+      CommandManager().getCommand(message[0])?.executeCommand(
+         channel = UserData(event.channel.name, event.channel.id),
+         sender = UserData(event.user.name, event.user.id),
+         args = message.subList(1, message.size)
+      )
    }
 }
