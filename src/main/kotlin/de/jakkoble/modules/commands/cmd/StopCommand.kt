22c1c7000a636878f36ab8ba53263538c0e0d46e
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-10-20 10:33:57 +0200

    Removed redundant Message 🧹

--- src/main/kotlin/de/jakkoble/modules/commands/cmd/StopCommand.kt
+++ src/main/kotlin/de/jakkoble/modules/commands/cmd/StopCommand.kt
@@ -9,7 +9,6 @@ import kotlin.system.exitProcess
 
 class StopCommand: TwitchCommand("stop", true) {
    override fun onCommand(channel: UserData, sender: UserData, args: List<String>) {
-      TwitchBot.twitchClient.chat.sendMessage(channel.name, "${sender.displayName}, you have stopped the Bot.")
       ConsoleLogger.logInfo("The Bot is now stopping...")
       channels.map { it.userData.name }.forEach { TwitchBot.twitchClient.chat.leaveChannel(it) }
       TwitchBot.twitchClient.chat.disconnect()
