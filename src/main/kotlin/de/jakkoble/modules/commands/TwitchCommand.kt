c0edb520cfc5e5f2c26718847b2c04fe2bade8f1
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-10-20 10:15:54 +0200

    Added Feedback Message 📰

--- src/main/kotlin/de/jakkoble/modules/commands/TwitchCommand.kt
+++ src/main/kotlin/de/jakkoble/modules/commands/TwitchCommand.kt
@@ -1,7 +1,9 @@
 package de.jakkoble.modules.commands
 
+import de.jakkoble.modules.commands.cmd.ChannelCommand
 import de.jakkoble.modules.core.TwitchBot
 import de.jakkoble.modules.data.UserData
+import de.jakkoble.modules.data.getChannelDataByID
 import de.jakkoble.utils.ConsoleLogger
 
 abstract class TwitchCommand(cmd: String, ownerOnly: Boolean) {
@@ -14,6 +16,10 @@ abstract class TwitchCommand(cmd: String, ownerOnly: Boolean) {
    fun executeCommand(channel: UserData, sender: UserData, args: List<String>) {
       if (ownerCommand && sender.id != "205919808") {
          TwitchBot.twitchClient.chat.sendMessage(channel.name, "${sender.displayName}, you don't have enough permission to execute this Command!")
+         return
+      }
+      if (getChannelDataByID(channel.id)?.enabled == false && command != ChannelCommand().command) {
+         TwitchBot.twitchClient.chat.sendMessage(channel.name, "${sender.displayName}, the Bot is currently disabled. Ask Jakkoble to enable it again.")
          return
       }
       ConsoleLogger.logInfo("${channel.displayName}: User ${sender.displayName} executed $command Command.")
