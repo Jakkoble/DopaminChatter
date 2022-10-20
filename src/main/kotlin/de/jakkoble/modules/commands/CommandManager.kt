9c8363e4fe133286bd2a1bc01d22aec3ef6a29a1
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-10-20 08:56:06 +0200

    Registered New Commands ✅

--- src/main/kotlin/de/jakkoble/modules/commands/CommandManager.kt
+++ src/main/kotlin/de/jakkoble/modules/commands/CommandManager.kt
@@ -1,11 +1,15 @@
 package de.jakkoble.modules.commands
 
 import de.jakkoble.modules.commands.cmd.ChannelCommand
+import de.jakkoble.modules.commands.cmd.EmoteCommand
+import de.jakkoble.modules.commands.cmd.StopCommand
 
 private val commands: MutableList<TwitchCommand> = mutableListOf()
 class CommandManager {
    fun registerCommands() {
       commands.add(ChannelCommand())
+      commands.add(EmoteCommand())
+      commands.add(StopCommand())
    }
    fun getCommand(command: String): TwitchCommand? = commands.firstOrNull { it.command.equals(command, true) }
 }
