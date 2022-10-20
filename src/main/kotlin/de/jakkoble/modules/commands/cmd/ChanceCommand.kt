13deea5541e3507c5bd62733f5cb2011063bd2d5
Jakkoble <business@jakkoble.de>
2022-10-20 21:26:58 +0200

    Added Chance Command Functionality ✅

--- src/main/kotlin/de/jakkoble/modules/commands/cmd/ChanceCommand.kt
+++ src/main/kotlin/de/jakkoble/modules/commands/cmd/ChanceCommand.kt
@@ -1,10 +1,20 @@
 package de.jakkoble.modules.commands.cmd
 
 import de.jakkoble.modules.commands.TwitchCommand
-import de.jakkoble.modules.data.UserData
+import de.jakkoble.modules.core.TwitchBot
+import de.jakkoble.modules.data.*
 
 class ChanceCommand: TwitchCommand("chance", false) {
    override fun onCommand(channel: UserData, sender: UserData, args: List<String>) {
-
+      if (args.size != 1) return
+      val chance = args[0]
+      val data = getChannelDataByID(channel.id)
+      data?.update(ChannelData(
+         userData = channel,
+         enabled = data.enabled,
+         customEmotes = data.customEmotes,
+         writingChance = chance.toInt()
+      ))
+      TwitchBot.twitchClient.chat.sendMessage(channel.name, "${sender.displayName}, you have set the WritingChance to $chance%.")
    }
 }
