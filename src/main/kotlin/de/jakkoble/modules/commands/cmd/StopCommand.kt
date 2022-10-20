c5c5bfea5ad65c4afdd528954b8fa9c0ee71c968
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-10-20 09:40:40 +0200

    Added Feedback Message 📰

--- src/main/kotlin/de/jakkoble/modules/commands/cmd/StopCommand.kt
+++ src/main/kotlin/de/jakkoble/modules/commands/cmd/StopCommand.kt
@@ -4,10 +4,13 @@ import de.jakkoble.modules.commands.TwitchCommand
 import de.jakkoble.modules.core.TwitchBot
 import de.jakkoble.modules.data.UserData
 import de.jakkoble.modules.data.channels
+import de.jakkoble.utils.ConsoleLogger
 import kotlin.system.exitProcess
 
 class StopCommand: TwitchCommand("stop", true) {
    override fun onCommand(channel: UserData, sender: UserData, args: List<String>) {
+      TwitchBot.twitchClient.chat.sendMessage(channel.name, "${sender.displayName}, you have stopped the Bot.")
+      ConsoleLogger.logInfo("The Bot is now stopping...")
       channels.map { it.userData.name }.forEach { TwitchBot.twitchClient.chat.leaveChannel(it) }
       TwitchBot.twitchClient.chat.disconnect()
       TwitchBot.twitchClient.close()
