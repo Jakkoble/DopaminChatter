b67af030d5b35940818a19e29d6d32b04cf7fdf7
Jakkoble <business@jakkoble.de>
2022-10-21 16:53:51 +0200

    Added UnregisterCommand 🔓

---
+++ src/main/kotlin/de/jakkoble/modules/commands/cmd/UnregisterCommand.kt
@@ -0,0 +1,25 @@
+package de.jakkoble.modules.commands.cmd
+
+import de.jakkoble.modules.commands.TwitchCommand
+import de.jakkoble.modules.core.TwitchBot
+import de.jakkoble.modules.data.DataManager
+import de.jakkoble.modules.data.UserData
+import de.jakkoble.modules.data.channels
+
+class UnregisterCommand: TwitchCommand("unregister", false) {
+   override fun onCommand(channel: UserData, sender: UserData, args: List<String>) {
+      if (channel.id != "205919808") {
+         TwitchBot.twitchClient.chat.sendMessage(channel.name, "${sender.displayName}, this Command is only available in Jakkoble's Twitch Chat ( https://twitch.tv/jakkoble )")
+         return
+      }
+      val channelData = channels.firstOrNull { it.userData.id == sender.id }
+      if (channelData == null) {
+         TwitchBot.twitchClient.chat.sendMessage(channel.name, "${sender.displayName}, the Bot is already removed from your Channel. Type #register to add it again.")
+         return
+      }
+      channels.remove(channelData)
+      DataManager.updateChannelData()
+      TwitchBot.twitchClient.chat.leaveChannel(sender.name)
+      TwitchBot.twitchClient.chat.sendMessage(channel.name, "${sender.displayName}, the Bot is now removed from your Channel.")
+   }
+}
