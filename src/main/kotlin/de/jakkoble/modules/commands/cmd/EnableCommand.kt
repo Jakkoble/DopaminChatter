df203716df65d6e786b7992848ef2cb7f971ee66
Jakkoble <business@jakkoble.de>
2022-10-21 18:25:15 +0200

    Added EnableCommand 📝

---
+++ src/main/kotlin/de/jakkoble/modules/commands/cmd/EnableCommand.kt
@@ -0,0 +1,23 @@
+package de.jakkoble.modules.commands.cmd
+
+import de.jakkoble.modules.commands.TwitchCommand
+import de.jakkoble.modules.core.TwitchBot
+import de.jakkoble.modules.data.ChannelData
+import de.jakkoble.modules.data.UserData
+import de.jakkoble.modules.data.getChannelDataByID
+import de.jakkoble.modules.data.update
+import de.jakkoble.utils.ConsoleLogger
+
+class EnableCommand: TwitchCommand("enable", false, true) {
+   override fun onCommand(channel: UserData, sender: UserData, args: List<String>) {
+      val channelData = getChannelDataByID(channel.id) ?: return
+      channelData.update(
+         ChannelData(
+         userData = channel,
+         customEmotes = channelData.customEmotes,
+         writingChance = channelData.writingChance)
+      )
+      TwitchBot.twitchClient.chat.sendMessage(channel.name, "${sender.displayName}, the Bot is now enabled.")
+      ConsoleLogger.logInfo("Enabled Bot for Channel '${channel.name}'.")
+   }
+}
