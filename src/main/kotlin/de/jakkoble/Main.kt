c727a776087a3917846c5e9c4c3baa03403728c2
Jakkoble <jakob.wassertheurer@eco-landeck.at>
2022-10-21 09:34:10 +0200

    Added Token Check ✅

--- src/main/kotlin/de/jakkoble/Main.kt
+++ src/main/kotlin/de/jakkoble/Main.kt
@@ -5,8 +5,13 @@ import de.jakkoble.modules.core.TwitchBot
 import de.jakkoble.modules.data.DataManager
 import de.jakkoble.utils.ConsoleLogger
 import java.io.File
+import kotlin.system.exitProcess
 
 fun main() {
+   if (System.getenv("TOKEN") == null) {
+      ConsoleLogger.logWarning("Could not find Environmental Variable TOKEN.")
+      exitProcess(1)
+   }
    val startTime = System.currentTimeMillis()
    ConsoleLogger.logInfo("Started Bot in ${File("").absolutePath}...")
    DataManager.load()
