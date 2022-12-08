c6e2a099f47876fb3be0c7f8d0ad9aa07ab85db2
Jakkoble <business@jakkoble.de>
2022-12-08 19:15:19 +0100

    Disabled Default Logging 📃

--- src/main/kotlin/de/jakkoble/Main.kt
+++ src/main/kotlin/de/jakkoble/Main.kt
@@ -1,17 +1,22 @@
 package de.jakkoble
 
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
 import de.jakkoble.modules.commands.CommandManager
 import de.jakkoble.modules.core.TwitchBot
 import de.jakkoble.modules.data.DataManager
 import de.jakkoble.utils.ConsoleLogger
 import java.io.File
 import kotlin.system.exitProcess
-
+import org.slf4j.LoggerFactory
 fun main() {
    if (System.getenv("TOKEN") == null) {
       ConsoleLogger.logWarning("Could not find Environmental Variable TOKEN.")
       exitProcess(1)
    }
+   (LoggerFactory.getILoggerFactory().getLogger("com.netflix.config") as Logger).level = Level.OFF
+   (LoggerFactory.getILoggerFactory().getLogger("io.github.xanthic.cache.core.CacheApiSettings") as Logger).level = Level.OFF
+   (LoggerFactory.getILoggerFactory().getLogger("com.github.twitch4j.chat.TwitchChat") as Logger).level = Level.OFF
    val startTime = System.currentTimeMillis()
    ConsoleLogger.logInfo("Started Bot in ${File("").absolutePath}...")
    DataManager.load()
